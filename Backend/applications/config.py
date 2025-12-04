8105bf4026c73cbebbd76f40fdb6d213f7bdfd90
vivekanand kumawat <vivekanandkumawat262@gmail.com>
2025-12-04 12:03:17 +0530

    Remove Backend & Frontend, add submission zip

--- Backend/applications/config.py
+++
@@ -1,9 +0,0 @@
-class Config():
-    DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class LocalDevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///vehicle_parking.sqlite3"
-    JWT_SECRET_KEY = "2f8b51a28371090fb2d7b1377489814c"
